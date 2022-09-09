2dab53f51708bb578c8fd5a68902bdd67a769ec6
Despan Alex <gargant@LAPTOP-450TNJ1O.localdomain>
2022-09-09 18:01:15 +0200

    commented code

--- output.s
+++ output.s
@@ -7,14 +7,14 @@ output: .asciz "First number is %ld and second number is %ld\n"
 first: .asciz "%ld"
 second: .asciz "%ld"
 
-.global main     # making the main function visible
+.global main               # making the main function visible
 
 main: 
 	# prologue
 	push %rbp
 	mov %rsp, %rbp
 
-	call read # calling main function
+	call read          # calling main function
 		
 	# epilogue
 	movq %rbp, %rsp
@@ -22,8 +22,7 @@ main:
 
 end:
 	movq $0, %rdi
-	call exit
-	
+	call exit	
 
 read:
 	# prologue
@@ -31,15 +30,17 @@ read:
 	mov %rsp, %rbp
 	
 	# showing base prompt
-	movq $0, %rax
+	movq $0, %rax  
 	movq $prompt_first, %rdi
 	call printf
 
-	subq $16, %rsp # allocating space for input
-	
-	movq $0, %rax
-	movq $first, %rdi
-	leaq -16(%rbp), %rsi
+	subq $16, %rsp       # allocating space for input
+	              	     # (highly recommend understanding how a stack works if this
+			     # doesn't make sense)
+	movq $0, %rax        
+	movq $first, %rdi    # moving $first address to rdi
+	leaq -16(%rbp), %rsi # loading effective address of -16 from rbp (where we 
+			     # started allocating memory) to rsi (argument to store input)
 	call scanf
 		
 	# showing power prompt
@@ -49,11 +50,13 @@ read:
 	
 	movq $0, %rax
 	movq $second, %rdi
-	leaq -8(%rbp), %rsi
+	leaq -8(%rbp), %rsi  # check code above ^
 	call scanf
 
-	movq -16(%rbp), %rsi
-	movq -8(%rbp), %rdx
+	movq -16(%rbp), %rsi # copying the first input from the stack to rsi
+	movq -8(%rbp), %rdx  # copying the second input from the stack to rdx
+			     # (check lab manual by CTRL + Fing Parameters and you'll
+			     # find the order of registers for args)
 	movq $0, %rax
 	movq $output, %rdi
 	call printf
